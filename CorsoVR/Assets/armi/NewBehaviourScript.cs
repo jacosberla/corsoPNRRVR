53d6686e097e1ebf3e8a3d8f1f228b7717193558
jacosberla <jacopo.tocchio@gmail.com>
2025-02-13 15:28:33 +0100

    C#

--- CorsoVR/Assets/armi/NewBehaviourScript.cs
+++ CorsoVR/Assets/armi/NewBehaviourScript.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class KunaiSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // L'asset del proiettile
+    [serializeField]
+    private GameObjet proiettile;
+ 
+    // il punto da dove parte il proiettile
+    [serializeField]
+    private Transform firePoint;
+
+    //L'azione che l'utente fa per sparare
+    [serializeField]
+    private InputActionReference shootAction;
+
+   
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*
+         shootAction.action.WasPressedThisFrame() =>
+         se ho premuto in questo frame da come valore vero 
+         altrimenti da come valore falso
+        */
+        if(shootAction.action.WasPressedThisFrame())
+        {
+            /*
+              Instantiate => Crea
+              crea il proiettil nella posizione firePoint.position, con firePoint.rotation
+
+            */
+           Instantiate(proiettile, firePoint.position, firePoint.rotation)
+        }
     }
 }
